0c1945bfb6287733542af0ccba9819dab27168ee
Andrew Cottam <a.cottam@gmail.com>
2017-04-11 08:09:37 +0000

    google api authorising and requesting complete

--- gee_cart_tools/scripts/gee_cart_tools.js
+++ gee_cart_tools/scripts/gee_cart_tools.js
@@ -1,5 +1,12 @@
-require(["dojo/dom", "dojo/html", "dojo/request/script", "dojo/parser", "dojo/ready", "dijit/layout/ContentPane", "dijit/layout/BorderContainer"],
-    function(dom, html, script, parser, ready) {
+require({
+        async: true,
+        packages: [{
+                name: "widgetsPath",
+                location: "/../../widgets"
+            } //i.e. up 2 levels from index.html 
+        ]
+    }, ["widgetsPath/googleApiClient", "dojo/dom", "dojo/html", "dojo/request/script", "dojo/parser", "dojo/ready", "dijit/layout/ContentPane", "dijit/layout/BorderContainer"],
+    function(GoogleApiClient, dom, html, script, parser, ready) {
         var geeImageServerUrl = "https://geeImageServer.appspot.com";
         ready(function() {
             parser.parse().then(function() {
@@ -13,17 +20,13 @@ require(["dojo/dom", "dojo/html", "dojo/request/script", "dojo/parser", "dojo/re
                 createCartTree(cart);
             });
 
-// <script src="https://apis.google.com/js/platform.js" async defer></script> <!-- Google Sign-In for Website Library-->
-// <script src="https://apis.google.com/js/api.js"></script> <!-- Google API Javascript Client Library-->
-// GoogleAuth, isAuthorized, currentApiRequest = {
-//     'path': '/fusiontables/v2/tables/14hPdnqjG33cm1bF6fdkGRrgt1Sh1fpzXauO2NZ-5'
-// };
-// clientId: '537433629273-q2f0on91cnsuuckkmtl0pbk24mkg6e3m.apps.googleusercontent.com',
-// scope: 'https://www.googleapis.com/auth/fusiontables https://www.googleapis.com/auth/fusiontables.readonly'
-//     'path': '/fusiontables/v2/tables/14hPdnqjG33cm1bF6fdkGRrgt1Sh1fpzXauO2NZ-5'
-
             function createCartTree(cart) {
                 html.set(dom.byId("cartTreeAsR"), cart.tree);
+                var client = new GoogleApiClient();
+                client.authorise('537433629273-q2f0on91cnsuuckkmtl0pbk24mkg6e3m.apps.googleusercontent.com', 'https://www.googleapis.com/auth/fusiontables https://www.googleapis.com/auth/fusiontables.readonly');
+                client.request('/fusiontables/v2/tables/14hPdnqjG33cm1bF6fdkGRrgt1Sh1fpzXauO2NZ-5').then(function(response) {
+                    console.log(response);
+                });
             }
         });
     });
