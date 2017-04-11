425b3844e2e739e7720378f88bfd0ce3505cea20
Andrew Cottam <a.cottam@gmail.com>
2017-04-11 10:58:12 +0000

    select boxes for columns in fusion tables

--- gee_cart_tools/scripts/gee_cart_tools.js
+++ gee_cart_tools/scripts/gee_cart_tools.js
@@ -5,23 +5,11 @@ require({
                 location: "/../../widgets"
             } //i.e. up 2 levels from index.html 
         ]
-    }, ["dijit/form/Select", "dojo/store/Memory", "widgetsPath/googleApiClient", "dojo/dom", "dojo/html", "dojo/request/script", "dojo/parser", "dojo/ready", "dijit/layout/ContentPane", "dijit/layout/BorderContainer"],
-    function(Select, Memory, GoogleApiClient, dom, html, script, parser, ready) {
+    }, ["dijit/form/Button", "dijit/registry", "dojo/_base/array", "dijit/form/Select", "dojo/store/Memory", "widgetsPath/googleApiClient", "dojo/dom", "dojo/html", "dojo/request/script", "dojo/parser", "dojo/ready", "dijit/layout/ContentPane", "dijit/layout/BorderContainer"],
+    function(Button, registry, array, Select, Memory, GoogleApiClient, dom, html, script, parser, ready) {
         var geeImageServerUrl = "https://geeImageServer.appspot.com";
         ready(function() {
             parser.parse().then(function() {
-
-            }); //startup any widgets in the page
-
-            script.get(geeImageServerUrl + "/getCartTree", {
-                jsonp: "callback"
-            }).then(function(response) {
-                var cart = (response) && (response.records);
-                createCartTree(cart);
-            });
-
-            function createCartTree(cart) {
-                html.set(dom.byId("cartTreeAsR"), cart.tree);
                 var client = new GoogleApiClient();
                 client.authorise('537433629273-q2f0on91cnsuuckkmtl0pbk24mkg6e3m.apps.googleusercontent.com', 'https://www.googleapis.com/auth/fusiontables https://www.googleapis.com/auth/fusiontables.readonly');
                 // client.request('/fusiontables/v2/tables/14hPdnqjG33cm1bF6fdkGRrgt1Sh1fpzXauO2NZ-5').then(function(response) {
@@ -29,8 +17,9 @@ require({
                 // });
                 client.request('fusiontables/v2/tables', {
                     maxResults: 100,
-                }).then(function(response) {
-                    // create store instance referencing data from states.json
+                }).then(populateFusionTableSelect);
+
+                function populateFusionTableSelect(response) {
                     var fusionTablesStore = new Memory({
                         idProperty: "tableId",
                         data: response.result.items
@@ -42,13 +31,85 @@ require({
                         style: "width: 200px;",
                         labelAttr: "name",
                         maxHeight: -1, // tells _HasDropDown to fit menu within viewport
-                        onChange: function(value) {
-                            document.getElementById("value").innerHTML = value;
-                            document.getElementById("displayedValue").innerHTML = this.get("displayedValue");
-                        }
-                    }, "fusionTablesStore");
+                        onChange: fusionTableChanged,
+                    }, dom.byId("fusionTables"));
                     select.startup();
-                })
+                }
+
+                function fusionTableChanged(value) {
+                    client.request('fusiontables/v2/tables/' + value).then(getColumnNames);
+                }
+
+                function getColumnNames(response) {
+                    if (response.result.columns) {
+                        var filteredColumnNames = array.filter(response.result.columns, function(item) {
+                            return item.name != "geometry";
+                        });
+                        var columnNamesStore = new Memory({
+                            idProperty: "name",
+                            data: filteredColumnNames
+                        });
+                        populateColumnNamesSelect(columnNamesStore);
+                    };
+                }
+
+                function populateColumnNamesSelect(columnNamesStore) {
+                    createColumnSelect("x", columnNamesStore);
+                    createColumnSelect("y", columnNamesStore);
+                    createGoButton();
+                }
+
+                function createColumnSelect(axis, columnNamesStore) {
+                    if (registry.byId(axis + "ColumnName")) {
+                        registry.byId(axis + "ColumnName").setStore(columnNamesStore);
+                    }
+                    else {
+                        // create Select widget, populating its options from the store
+                        var select = new Select({
+                            name: axis + "ColumnNameSelect",
+                            store: columnNamesStore,
+                            style: "width: 200px;",
+                            labelAttr: "name",
+                            maxHeight: -1, // tells _HasDropDown to fit menu within viewport
+                        }, dom.byId(axis + "ColumnName"));
+                        select.startup();
+                    }
+                }
+
+                function createGoButton() {
+                    if (!registry.byId("goButton")) {
+                        new Button({
+                            label: "Go",
+                            onClick: getData,
+                        }, "goButton").startup();
+                    }
+                }
+
+                function getData() {
+                    var xColumnName = registry.byId("xColumnName").value;
+                    var yColumnName = registry.byId("yColumnName").value;
+                    var tableid = registry.byId("fusionTables").value;
+                    client.request('fusiontables/v2/query', {
+                        sql: 'select ' + xColumnName + ',' + yColumnName + ' from ' + tableid,
+                    }).then(function(response) {
+                        populateChart(response);
+                    });
+                }
+
+                function populateChart(response) {
+                    console.log(response.result.rows);
+                }
+            });
+
+            script.get(geeImageServerUrl + "/getCartTree", {
+                jsonp: "callback"
+            }).then(function(response) {
+                var cart = (response) && (response.records);
+                createCartTree(cart);
+            });
+
+            function createCartTree(cart) {
+                html.set(dom.byId("cartTreeAsR"), cart.tree);
             }
         });
     });
