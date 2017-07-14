800ce180208685bd068b619c088469eedf60934c
Andrew Cottam <a.cottam@gmail.com>
2017-07-14 15:42:53 +0000

    leaflet map - added callouts

--- leaflet_map/scripts/leaflet_map.js
+++ leaflet_map/scripts/leaflet_map.js
@@ -6,14 +6,20 @@ require({
 	}, ["dojo/dom-style", "dojo/html", "dojo/dom-construct", "dojo/_base/array", "dojo/_base/lang", "dijit/form/Button", "dojo/dom", "widgetsPackage/ReferenceList", "widgetsPackage/ImageryTimeSlider", "widgetsPackage/PhotoViewer", "dojo/dom-construct", "dojo/io-query", "dojo/_base/window", "dojo/parser", "dojo/on", "dijit/registry", "dojo/ready", "dijit/form/CheckBox", "dijit/layout/BorderContainer", "dijit/layout/ContentPane"],
 	function(domStyle, html, domConstruct, array, lang, Button, dom, ReferenceList, ImageryTimeSlider, PhotoViewer, domConstruct, ioQuery, win, parser, on, registry, ready) {
 		ready(function() {
+			var CALLOUT_CIRCLE_RADIUS = 7;
+			var CALLOUT_LINE_LENGTH = 50;
+			var FILL_OPACITY = 0.2;
+			var OPACITY = 0.4;
+			// var FILL_OPACITY = 0;
+			// var OPACITY = 0;
 			var vectorTileStyling = {
 				water: {
 					fill: true,
 					weight: 1,
 					fillColor: '#06cccc',
 					color: '#06cccc',
-					fillOpacity: 0.2,
-					opacity: 0.4,
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY,
 				},
 				boundaries: function(properties, zoom) {
 					var style;
@@ -23,8 +29,8 @@ require({
 								weight: 100,
 								fillColor: 'pink',
 								color: 'pink',
-								fillOpacity: 0.2,
-								opacity: 0.4
+								fillOpacity: FILL_OPACITY,
+								opacity: OPACITY
 							};
 							break;
 						default:
@@ -32,8 +38,8 @@ require({
 								weight: 1,
 								fillColor: 'pink',
 								color: 'pink',
-								fillOpacity: 0.2,
-								opacity: 0.4
+								fillOpacity: FILL_OPACITY,
+								opacity: OPACITY
 							};
 							break;
 					}
@@ -43,46 +49,46 @@ require({
 					weight: 1,
 					fillColor: '#2375e0',
 					color: '#2375e0',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				landcover: {
 					fill: true,
 					weight: 1,
 					fillColor: '#53e033',
 					color: '#53e033',
-					fillOpacity: 0.2,
-					opacity: 0.4,
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY,
 				},
 				landuse: {
 					fill: true,
 					weight: 1,
 					fillColor: '#e5b404',
 					color: '#e5b404',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				park: {
 					fill: true,
 					weight: 1,
 					fillColor: '#84ea5b',
 					color: '#84ea5b',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				boundary: {
 					weight: 1,
 					fillColor: '#c545d3',
 					color: '#c545d3',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				aeroway: {
 					weight: 1,
 					fillColor: '#51aeb5',
 					color: '#51aeb5',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				roads: function(properties, zoom) {
 					var style;
@@ -92,8 +98,8 @@ require({
 								weight: 1,
 								fillColor: '#f2b648',
 								color: '#f2b648',
-								fillOpacity: 0.2,
-								opacity: 0.4,
+								fillOpacity: FILL_OPACITY,
+								opacity: OPACITY,
 							};
 							break;
 						default:
@@ -101,8 +107,8 @@ require({
 								weight: 1,
 								fillColor: '#f2b648',
 								color: '#f2b648',
-								fillOpacity: 0.2,
-								opacity: 0.4,
+								fillOpacity: FILL_OPACITY,
+								opacity: OPACITY,
 							};
 							break;
 					}
@@ -112,32 +118,32 @@ require({
 					weight: 0.5,
 					fillColor: '#f2b648',
 					color: '#f2b648',
-					fillOpacity: 0.2,
-					opacity: 0.4,
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY,
 					// 					dashArray: [4, 4]
 				},
 				bridge: { // mapbox only
 					weight: 0.5,
 					fillColor: '#f2b648',
 					color: '#f2b648',
-					fillOpacity: 0.2,
-					opacity: 0.4,
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY,
 					// 					dashArray: [4, 4]
 				},
 				transportation: { // openmaptiles only
 					weight: 0.5,
 					fillColor: '#f2b648',
 					color: '#f2b648',
-					fillOpacity: 0.2,
-					opacity: 0.4,
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY,
 					// 					dashArray: [4, 4]
 				},
 				transit: { // mapzen only
 					weight: 0.5,
 					fillColor: '#f2b648',
 					color: '#f2b648',
-					fillOpacity: 0.2,
-					opacity: 0.4,
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY,
 					// 					dashArray: [4, 4]
 				},
 				building: {
@@ -145,54 +151,45 @@ require({
 					weight: 1,
 					fillColor: '#2b2b2b',
 					color: '#2b2b2b',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				water_name: {
 					weight: 1,
 					fillColor: '#022c5b',
 					color: '#022c5b',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				transportation_name: {
 					weight: 1,
 					fillColor: '#bc6b38',
 					color: '#bc6b38',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				place: {
 					weight: 1,
 					fillColor: '#f20e93',
 					color: '#f20e93',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				housenumber: {
 					weight: 1,
 					fillColor: '#ef4c8b',
 					color: '#ef4c8b',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
 				poi: {
 					weight: 1,
 					fillColor: '#3bb50a',
 					color: '#3bb50a',
-					fillOpacity: 0.2,
-					opacity: 0.4
+					fillOpacity: FILL_OPACITY,
+					opacity: OPACITY
 				},
-				earth: { // mapzen only
-					fill: true,
-					weight: 1,
-					fillColor: '#c0c0c0',
-					color: '#c0c0c0',
-					fillOpacity: 0.2,
-					opacity: 0.4,
-					className: "noDisplay", //added by ac on 14/7/17
-				},
-
+				earth: [], //added by ac on 14/7/17 so that mouse over events can occur under this layer - this turns it off
 
 				// Do not symbolize some stuff for mapbox
 				country_label: [],
@@ -315,41 +312,63 @@ require({
 				vectorGridLayer.popup = domConstruct.create("div", {
 					id: "popup"
 				}, win.body());
+				vectorGridLayer.continuous = true;
 				on(vectorGridLayer, "mouseover", lang.hitch(this, function(evt) {
-					domStyle.set(dom.byId("popup"), "left", evt.originalEvent.clientX + "px");
-					domStyle.set(dom.byId("popup"), "top", evt.originalEvent.clientY + "px");
+					if (vectorGridLayer.continuous) {
+						showPopup(evt);
+					}
+					else {
+
+					}
+				}));
+
+				function showPopup(evt) {
+					//show the callout
+					vectorGridLayer.currentId = evt.layer.properties.id;
+					// vectorGridLayer.setFeatureStyle(evt.layer.properties.class, {
+					// 	stroke: "#ffffff",
+					// 		fill: "#ffffff",
+					// });
+					//add the circle
+					if (vectorGridLayer.circle) {
+						vectorGridLayer.circle.remove();
+						vectorGridLayer.polyline.remove();
+					}
+					vectorGridLayer.circle = L.circleMarker(evt.latlng, {
+						radius: CALLOUT_CIRCLE_RADIUS,
+						color: "white",
+						weight: 1,
+						fillOpacity: 0,
+					});
+					var p1 = evt.containerPoint;
+					var linePoints = [
+						[p1.x - CALLOUT_CIRCLE_RADIUS, p1.y],
+						[p1.x - CALLOUT_CIRCLE_RADIUS - CALLOUT_LINE_LENGTH, p1.y],
+						[p1.x - CALLOUT_CIRCLE_RADIUS - CALLOUT_LINE_LENGTH, p1.y - CALLOUT_LINE_LENGTH]
+					];
+					var latlngs = [];
+					array.forEach(linePoints, function(point) {
+						latlngs.push(map.containerPointToLatLng(point));
+					});
+					vectorGridLayer.circle.addTo(map);
+					vectorGridLayer.polyline = L.polyline(latlngs, {
+						color: 'white',
+						weight: 1,
+					}).addTo(map);
+					//show the properties
 					var text = "";
+					var omitProps = ["sort_rank", "kind_detail", "id:right", "id:left", "source", "min_zoom", "id", "osm_relation", "area", "tier", "boundary"];
 					for (var prop in evt.layer.properties) {
-						text += "<div>" + prop + ":" + evt.layer.properties[prop] + "</div>";
+						if (omitProps.indexOf(prop) == -1) {
+							if (evt.layer.properties[prop]) {
+								text += "<div>" + evt.layer.properties[prop].substr(0, 1).toUpperCase() + evt.layer.properties[prop].substr(1) + "</div>";
+							}
+						}
 					}
 					html.set(dom.byId("popup"), text);
-					// console.log(evt.layer._renderer._container);
-					// vectorGridLayer.text = "leafletId:" + evt.layer._leaflet_id + " minX:" + evt.layer._pxBounds.min.x + " maxX:" + evt.layer._pxBounds.max.x + " minY:" + evt.layer._pxBounds.min.y + " maxY:" + evt.layer._pxBounds.max.y;
-					// if (vectorGridLayer.text != vectorGridLayer.lastText) {
-					// 	vectorGridLayer.setFeatureStyle(evt.layer.properties.class, {
-					// 		stroke: "#ffffff",
-					// 		fill: "#ffffff",
-					// 	});
-					// 	vectorGridLayer.lastText = vectorGridLayer.text;
-					// array.forEach(evt.layer._parts[0], function(point) {
-					// 	evt.layer.circle = L.circle(map.layerPointToLatLng(point), {
-					// 		radius: 2,
-					// 		color: "white",
-					// 		weight: 1,
-					// 	});
-					// 	evt.layer.circle.addTo(map);
-					// });
-					// }
-					// var x = evt.layer._pxBounds.min.x + ((evt.layer._pxBounds.max.x - evt.layer._pxBounds.min.x) / 2);
-					// var y = evt.layer._pxBounds.min.y + ((evt.layer._pxBounds.max.y - evt.layer._pxBounds.min.y) / 2);
-					// var featureCentroid = map.layerPointToLatLng([x, y]);
-					// evt.layer.circle = L.circle(featureCentroid, {
-					// 	radius: 300,
-					// 	color: "white",
-					// 	weight: 1,
-					// });
-					// evt.layer.circle.addTo(map);
-				}));
+					domStyle.set(dom.byId("popup"), "left", (evt.originalEvent.clientX - CALLOUT_CIRCLE_RADIUS - CALLOUT_LINE_LENGTH - (domStyle.get(dom.byId("popup"), "width") / 2) - 20) + "px"); //20 for padding
+					domStyle.set(dom.byId("popup"), "top", (evt.originalEvent.clientY - CALLOUT_LINE_LENGTH - domStyle.get(dom.byId("popup"), "height") - 22) + "px"); //20 for padding
+				}
 			}));
 		});
 	});
