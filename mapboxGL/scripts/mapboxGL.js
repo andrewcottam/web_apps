a0b488fd60684a3187c76e73e1b8c1b96de60f19
Andrew Cottam <a.cottam@gmail.com>
2017-08-21 09:01:35 +0000

    mapboxgl - example of local style with my own tippeecanoe tiles rendered

--- mapboxGL/scripts/mapboxGL.js
+++ mapboxGL/scripts/mapboxGL.js
@@ -1,23 +1,36 @@
-require(["dojo/io-query", "dojo/request/script", "dojo/on", "node_modules/mapbox-gl/dist/mapbox-gl.js", "scripts/mapbox-gl-geocoder.min.js"],
-    function(ioQuery, script, on, mapboxgl, MapboxGeocoder) {
+require(["dojo/_base/lang", "dojo/_base/array", "dojo/io-query", "dojo/request/script", "dojo/on", "node_modules/mapbox-gl/dist/mapbox-gl.js", "scripts/mapbox-gl-geocoder.min.js", "scripts/mapbox-gl-inspect.min.js"],
+    function(lang, array, ioQuery, script, on, mapboxgl, MapboxGeocoder, MapboxInspect) {
+        var debug = true;
         mapboxgl.accessToken = 'pk.eyJ1IjoiYmxpc2h0ZW4iLCJhIjoiMEZrNzFqRSJ9.0QBRA2HxTb8YHErUFRMPZg'; //this is my access token
+        if (debug) {
+            console.debug("Using MapBoxGL version: " + mapboxgl.version);
+            console.debug("Go here for the version history: https://github.com/mapbox/mapbox-gl-js/releases");
+        }
         var map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v9',
-            // style: simple,
-            center: [-14.8975, 16.5887],
+            // style: "mapbox://styles/mapbox/streets-v10", //vanilla mapbox style
+            // style: "mapbox://styles/blishten/cj6f4n2j026qf2rnunkauikjm", //my style with a few minor edits including the salonga tileset
+            // style: "styles/localcopy.json", //straight copy of the previous mapbox studio authored style
+            style: "styles/localcopyplus.json", //straight copy of the previous mapbox studio authored style plus tippecanoe data on C9 and tilemaker data on google cloud storage
+            // style: "https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json", //openmaptile style
+            center: [-14.8975, 16.5887], //senegal
+            center: [21, -2], //salonga
             zoom: 12,
         });
+        //add the map controls
+        map.addControl(new MapboxInspect({
+            showInspectButton: debug,
+            popup: new mapboxgl.Popup({
+                closeButton: false,
+                closeOnClick: false
+            })
+        }));
         map.addControl(new mapboxgl.FullscreenControl());
-        var popup = new mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: false
-        });
         var geocoder = new MapboxGeocoder({
             accessToken: mapboxgl.accessToken,
-            flyTo: false,
+            flyTo: false, //this stops the geocoder panning to the result so we have to do it manually (below)
         });
-        //zoom to the location of selected feature
+        //zoom to the location of selected feature 
         geocoder.on("result", function(evt) {
             map.setCenter(evt.result.geometry.coordinates);
         });
@@ -34,14 +47,56 @@ require(["dojo/io-query", "dojo/request/script", "dojo/on", "node_modules/mapbox
                     if (data && data.length != 0) {
                         map.setCenter([data[0].lon, data[0].lat]);
                     }
-                }, function(err) {
-                    console.error(err);
                 });
             }
         });
-
         map.addControl(geocoder);
-        map.on('load', function() {
+        map.on("load", function(e) {
+            console.debug(e);
+            //to get the layer ids
+            // map.getStyle().layers;
+            // addLayerWDPA();
+            // addLayerTippeCanoe();
+            // addLayerSentinelHub();
+            // addLayerContours();
+        });
+        map.on("zoomend", function(e) {
+            console.debug("Zoom: " + map.getZoom());
+        });
+        map.on("styledata", function(e) {
+            // console.debug(e);
+        });
+        map.on("sourcedata", function(e) {
+            // console.debug(e);
+        });
+        map.on("mousemove", function(e) {
+            if (debug) {
+                var features = map.queryRenderedFeatures(e.point);
+                // console.debug(features);
+            }
+        });
+
+        //adds a vector layer produced by tippeecanoe on the vector-tiles C9 workspace 
+        function addLayerTippeCanoe() {
+            map.addLayer({
+                "id": "salongageojson",
+                "type": "fill",
+                layout: {
+                    visibility: "visible",
+                },
+                "source": {
+                    "type": 'vector',
+                    "tiles": ['https://vector-tiles-blishten.c9users.io/tilesets/{z}/{x}/{y}.pbf'],
+                },
+                "source-layer": "salongageojson",
+                "paint": {
+                    "fill-color": "rgba(99,148,69, 0.2)",
+                    "fill-outline-color": "rgba(87,131,61, 0.4)",
+                },
+            });
+        }
+
+        function addLayerWDPA() {
             map.addLayer({
                 "id": "wdpa",
                 "type": "fill",
@@ -55,18 +110,10 @@ require(["dojo/io-query", "dojo/request/script", "dojo/on", "node_modules/mapbox
                     "fill-outline-color": "rgba(87,131,61, 0.4)",
                 },
             });
-            map.on('mousemove', 'wdpa', function(e) {
-                map.getCanvas().style.cursor = 'pointer';
-                popup.setLngLat(e.lngLat)
-                    .setHTML(e.features[0].properties.NAME)
-                    .addTo(map);
-            });
-            map.on('mouseleave', 'wdpa', function() {
-                map.getCanvas().style.cursor = '';
-                popup.remove();
-            });
-            //sentinel hub parameters
-            var layers = "TRUE_COLOR";
+        }
+
+        function addLayerSentinelHub() {
+            //sentinel hub parameters - hard-coded for now
             var wmsParams = {
                 service: "WMS",
                 request: "GetMap",
@@ -94,6 +141,26 @@ require(["dojo/io-query", "dojo/request/script", "dojo/on", "node_modules/mapbox
                     'tileSize': 256
                 },
                 'paint': {}
-            }, 'aeroway-taxiway');
-        });
+            }, 'landuse_overlay_national_park');
+        }
+
+        function addLayerContours() {
+            map.addLayer({
+                "id": "terrain-data",
+                "type": "line",
+                "source": {
+                    type: 'vector',
+                    url: 'mapbox://mapbox.mapbox-terrain-v2'
+                },
+                "source-layer": "contour",
+                "layout": {
+                    "line-join": "round",
+                    "line-cap": "round"
+                },
+                "paint": {
+                    "line-color": "#ff69b4",
+                    "line-width": 1
+                }
+            });
+        }
     });
