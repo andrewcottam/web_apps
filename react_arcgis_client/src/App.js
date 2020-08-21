b5ff580dd2a4a9c63fe1206f60eb332c4035e43e
Andrew Cottam <a.cottam@gmail.com>
2020-08-21 13:15:48 +0000

    esri client to the OSM mirrored feature layers

--- react_arcgis_client/src/App.js
+++ react_arcgis_client/src/App.js
@@ -12,25 +12,44 @@ class App extends React.Component {
       zoom: 16
     });
     this.map.on('load', _ => {
-      this.map.addSource('podor', {
-        'type': 'geojson',
-        // return a single osm feature
-        // 'data': 'https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?where=osm_id2%3D%27832603018%27&f=pgeojson',
-        // return all features - returns 2000 features but not in the current view extent!
-        // 'data': 'https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?where=osm_id2%3E0&f=pgeojson',
-        // filter the osm features by geometry (in this case the port area in Podor, Senegal)
-        'data': 'https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?where=&objectIds=&time=&geometry=%7B%0D%0A++%22rings%22+%3A+%5B%5B%5B-1664900%2C+1880500%5D%2C+%5B-1664900%2C+1880600%5D%2C%0D%0A+++++++++++++++++++++++++++%5B-1664800%2C+1880600%5D%2C+%5B-1664800%2C+1880500%5D%2C%0D%0A+++++++++++++++++++++++++++%5B-1664900%2C+1880500%5D%5D%5D%2C%0D%0A++%22spatialReference%22+%3A+%7B%22wkid%22+%3A+102100%7D%0D%0A%7D&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=&bbox={bbox-epsg-3857}'
-      });
       this.map.addLayer({
-        'id': 'podor_Features',
+        'source': {
+          'type': 'geojson',
+          // return a single osm feature - geojson format
+          // 'data': 'https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?where=osm_id2%3D%27832603018%27&f=pgeojson',
+          
+          // return all features - returns 2000 features but not in the current view extent! - geojson format
+          // 'data': 'https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?where=osm_id2%3E0&f=pgeojson',
+          
+          // filter the osm features by geometry (in this case the port area in Podor, Senegal) - geojson format
+          'data': 'https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?geometry=%7B%0D%0A++%22rings%22+%3A+%5B%5B%5B-1664900%2C+1880500%5D%2C+%5B-1664900%2C+1880600%5D%2C%0D%0A+++++++++++++++++++++++++++%5B-1664800%2C+1880600%5D%2C+%5B-1664800%2C+1880500%5D%2C%0D%0A+++++++++++++++++++++++++++%5B-1664900%2C+1880500%5D%5D%5D%2C%0D%0A++%22spatialReference%22+%3A+%7B%22wkid%22+%3A+102100%7D%0D%0A%7D&geometryType=esriGeometryPolygon&f=pgeojson'
+        },
+        'id': 'podor_Features_geojson',
         'type': 'fill',
-        'source': 'podor',
         'layout': {},
         'paint': {
           'fill-color': '#088',
           'fill-opacity': 0.8
         }
       });
+      //the following source causes the pbf data to load, but it doesnt show it on the map - the pbf format is not compatible with mapbox vector format MVT which is used in vector tiles
+      // this.map.addLayer({
+      //   'id': 'podor_Features_pbf',
+      //   'type': 'fill',
+      //   'source': {
+      //     'type': 'vector',
+      //     // return a single osm feature - geojson format
+      //     'tiles': [
+      //       'https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?where=osm_id2%3D%27832603018%27&f=pbf',
+      //     ]
+      //   },
+      //   'source-layer': '',
+      //   'layout': {},
+      //   'paint': {
+      //     'fill-color': '#800',
+      //     'fill-opacity': 0.8
+      //   }
+      // });
     });
   }
   render() {
@@ -43,45 +62,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// https://services6.arcgis.com/Do88DoK2xjTUCXd1/ArcGIS/rest/services/OpenStreetMap_Buildings_for_AFR/FeatureServer/0/query?
-// where=osm_id2%3D%27832603018%27
-// &objectIds=
-// &time=
-// &geometry=
-// &geometryType=esriGeometryEnvelope
-// &inSR=
-// &spatialRel=esriSpatialRelIntersects
-// &resultType=none
-// &distance=0.0
-// &units=esriSRUnit_Meter
-// &returnGeodetic=false
-// &outFields=*
-// &returnGeometry=true
-// &returnCentroid=false
-// &featureEncoding=esriDefault
-// &multipatchOption=xyFootprint
-// &maxAllowableOffset=
-// &geometryPrecision=
-// &outSR=
-// &datumTransformation=
-// &applyVCSProjection=false
-// &returnIdsOnly=false
-// &returnUniqueIdsOnly=false
-// &returnCountOnly=false
-// &returnExtentOnly=false
-// &returnQueryGeometry=false
-// &returnDistinctValues=false
-// &cacheHint=false&orderByFields=
-// &groupByFieldsForStatistics=
-// &outStatistics=
-// &having=
-// &resultOffset=
-// &resultRecordCount=
-// &returnZ=false
-// &returnM=false
-// &returnExceededLimitFeatures=true
-// &quantizationParameters=
-// &sqlFormat=none
-// &f=pgeojson
-// &token=
