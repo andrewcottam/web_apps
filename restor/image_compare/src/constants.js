44124dadf5ea6555f654c35a2280d9416a3492ec
Andrew Cottam <a.cottam@gmail.com>
2022-04-15 09:01:31 +0000

    easter egg hunt 2022

--- restor/image_compare/src/constants.js
+++
@@ -1,6 +0,0 @@
-//application level global constants
-module.exports = Object.freeze({
-    PYTHON_REST_SERVER_ENDPOINT: "https://61c92e42cb1042699911c485c38d52ae.vfs.cloud9.eu-west-1.amazonaws.com:8081/python-rest-server/restor",
-    SEND_CREDENTIALS: true, //if true all post requests will send credentials
-    TIMEOUT: 0, //disable timeout setting
-});
