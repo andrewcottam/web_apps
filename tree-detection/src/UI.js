e8e51ddd5654603de1d550df6a6fd2b72128e319
Andrew Cottam <a.cottam@gmail.com>
2023-03-28 12:06:53 +0200

    tree detection can now change the model on the fly

--- tree-detection/src/UI.js
+++ tree-detection/src/UI.js
@@ -5,25 +5,17 @@ import IconButton from '@mui/material/IconButton';
 import DownloadIcon from '@mui/icons-material/Download';
 // import Checkbox from '@mui/material/Checkbox';
 import Sync from '@mui/icons-material/Sync';
-import {Map } from '@esri/react-arcgis';
-import {loadModules } from 'esri-loader';
+import { Map } from '@esri/react-arcgis';
+import { loadModules } from 'esri-loader';
 import GeojsonLayer from './GeojsonLayer.js'; // to hold the delineated tree crowns
 import GeeLayer from './GeeLayer.js'; // for the imagery coming from google earth engine
 import WaybackLayer from './WaybackLayer.js'; // for the imagery coming from esri wayback
 import TreeCrownMetrics from './TreeCrownMetrics.js';
-import { style } from '@mui/system';
+// import { style } from '@mui/system';
 
 class UI extends Component {
     constructor(props) {
         super(props);
-        // get the Cloud Run endpoint depending on which model is being specified in the queryparameter 'model'
-        let url = new URL(document.URL);
-        let query_params = url.searchParams;
-        let revision_tag = (query_params.has('model')) ? query_params.get('model') : 'cambridge';
-        let model_copyright = (revision_tag === 'cambridge') ? 'Closed-canopy CNN model source: Alejandro Coca-Castro, Matt Allen, 2021 Tree crown detection (The Enviromental AI Book). http://acocac.github.io/environmental-ai-book/forest/modelling/forest-modelling-treecrown_deepforest.html Online; accessed Mon Nov 01 2021' : 'Restor Tree Detection model 2023';
-        this.SERVER = (document.location.hostname === 'localhost') ? "http://localhost:8081" : "https://" + revision_tag + "---tree-detection-server-ny43uciwwa-oc.a.run.app"; //gcp microservices
-        this.GET_INSTANCES_IMAGE_ENDPOINT = this.SERVER + "/getInstancesImage";
-        this.GET_INSTANCES_ENDPOINT = this.SERVER + "/getInstances";
         //state
         this.state = {
             image_url: null, //bound to the image html element in the ui that shows the instances image or the raw image
@@ -40,19 +32,28 @@ class UI extends Component {
             score_range_value: [0, 800],
             gee_copyright: '© 2014 WWF Aerial Survey of the Congo. WWF/NASA JPL/KfW/BMUB/BMZ',
             wms_copyright: 'Imagery from OpenAerialMap. Maxar Products. WorldView2 © 2021 Maxar Technologies.',
-            model_copyright: model_copyright,
-            lng: 112.84350452926209, lat: -8.054735059174224, wms_endpoint: '' 
+            lng: 112.84350452926209, lat: -8.054735059174224, wms_endpoint: ''
         };
         //connect the map load event to set the state
         this.handleMapLoad = this.handleMapLoad.bind(this);
     }
 
+    setModel(model) {
+        // get the Cloud Run endpoint depending on which model is being specified
+        let model_copyright = (model === 'cambridge') ? 'Alejandro Coca-Castro, Matt Allen, 2021 Tree crown detection (The Enviromental AI Book). http://acocac.github.io/environmental-ai-book/forest/modelling/forest-modelling-treecrown_deepforest.html Online; accessed Mon Nov 01 2021' : 'Restor Tree Detection model 2023';
+        this.setState({ model: model, model_copyright: model_copyright });
+        this.SERVER = (document.location.hostname === 'localhost') ? "http://localhost:8081" : "https://" + model + "---tree-detection-server-ny43uciwwa-oc.a.run.app"; //gcp microservices
+        this.GET_INSTANCES_IMAGE_ENDPOINT = this.SERVER + "/getInstancesImage";
+        this.GET_INSTANCES_ENDPOINT = this.SERVER + "/getInstances";
+    }
     componentDidMount() {
         //load the ESRI javascript libraries
         loadModules(["esri/core/watchUtils", "esri/geometry/support/webMercatorUtils"]).then(([watchUtils, webMercatorUtils]) => {
             this.watchUtils = watchUtils;
             this.webMercatorUtils = webMercatorUtils;
         });
+        // set the initial model
+        this.setModel('cambridge');
     }
 
     handleMapLoad(map, view) {
@@ -75,17 +76,17 @@ class UI extends Component {
 
     changeToMaxarImage() {
         //move the map to java
-        this.state.view.goTo({ center: [ 112.84350452926209, -8.054735059174224], zoom: 18 }).then(_ => {
+        this.state.view.goTo({ center: [112.84350452926209, -8.054735059174224], zoom: 18 }).then(_ => {
             //set the state
-            this.setState({ mode: 'webtile_layer', status_text: 'Getting image from OpenAerialMap..', feature_collection: undefined, wms_endpoint:'https://tiles.openaerialmap.org/61b94e69b26de1000596d605/1/61b94e69b26de1000596d607/{z}/{x}/{y}', wms_copyright:'Imagery from OpenAerialMap. Maxar Products. WorldView2 © 2021 Maxar Technologies.' });
+            this.setState({ mode: 'webtile_layer', status_text: 'Getting image from OpenAerialMap..', feature_collection: undefined, wms_endpoint: 'https://tiles.openaerialmap.org/61b94e69b26de1000596d605/1/61b94e69b26de1000596d607/{z}/{x}/{y}', wms_copyright: 'Imagery from OpenAerialMap. Maxar Products. WorldView2 © 2021 Maxar Technologies.' });
         });
     }
 
-    changeToWaybackImage(){
+    changeToWaybackImage() {
         //move the map to northern ghana
         this.state.view.goTo({ center: [-2.503135, 10.064652], zoom: 18 }).then(_ => {
             //set the state
-            this.setState({ mode: 'webtile_layer', status_text: 'Getting image from Wayback..', feature_collection: undefined, wms_endpoint:'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/8249/{level}/{row}/{col}', wms_copyright:'ESRI Wayback'});
+            this.setState({ mode: 'webtile_layer', status_text: 'Getting image from Wayback..', feature_collection: undefined, wms_endpoint: 'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/WMTS/1.0.0/default028mm/MapServer/tile/8249/{level}/{row}/{col}', wms_copyright: 'ESRI Wayback' });
         });
     }
 
@@ -177,10 +178,10 @@ class UI extends Component {
             });
         });
     }
-    
-    downloadInstances(){
+
+    downloadInstances() {
         const file = new File([JSON.stringify(this.state.feature_collection)], 'tree_crowns.geojson', {
-          type: 'text/plain',
+            type: 'text/plain',
         });
         const link = document.createElement('a');
         const url = URL.createObjectURL(file);
@@ -227,6 +228,10 @@ class UI extends Component {
         this.setState({ score_range_value: value });
     }
 
+    //fired when the model changes
+    changeModel(value) {
+        this.setModel(value);
+    }
     render() {
         return (
             <div className="maindiv">
@@ -234,48 +239,57 @@ class UI extends Component {
                     <tbody>
                         <tr>
                             <td className={'imageCell'}>
-                                <input ref={input => this.inputElement = input} type="file" onChange={this.imageChosen.bind(this)} style={{'display':'none'}}/>
-                                <img src={this.state.image_url} className={"image"} alt='drone' style={{width:"700px",height:"700px", 'display': (this.state.image_url && this.state.mode === 'static_image') ? 'block' : 'none'}} onLoad={this.imageLoaded.bind(this)}/>
-                                <Map className={'esri_map'} onLoad={this.handleMapLoad} mapProperties={{ basemap: {portalItem: {id: "96cff8b8e48d45548833f19e29f09943"}}}} viewProperties={{center: [this.state.lng, this.state.lat], zoom: 19}} style={{height: this.state.mode === 'static_image' ? 1: 700}}>
-                            	    <GeeLayer detecting_tree_crowns={this.state.detecting_tree_crowns} blob_set={this.blob_set.bind(this)} visible={this.state.mode==='gee_layer'} layers={'WWF/carbon-maps/raw-data/imagery'} bands={"b1,b2,b3"} copyright={this.state.gee_copyright}/>
-                            	    <WaybackLayer detecting_tree_crowns={this.state.detecting_tree_crowns}  blob_set={this.blob_set.bind(this)} visible={this.state.mode==='webtile_layer'} urlTemplate={this.state.wms_endpoint} copyright={this.state.wms_copyright}/>
-                            	    <GeojsonLayer feature_collection={this.state.feature_collection} visible={this.state.mode!=='static_image' && this.state.show_crowns} show_boxes={this.state.show_boxes} show_masks={this.state.show_masks} show_scores={this.state.show_scores} show_areas={this.state.show_areas} area_range_value={this.state.area_range_value} score_range_value={this.state.score_range_value}/>
-                            	</Map>
+                                <input ref={input => this.inputElement = input} type="file" onChange={this.imageChosen.bind(this)} style={{ 'display': 'none' }} />
+                                <img src={this.state.image_url} className={"image"} alt='drone' style={{ width: "700px", height: "700px", 'display': (this.state.image_url && this.state.mode === 'static_image') ? 'block' : 'none' }} onLoad={this.imageLoaded.bind(this)} />
+                                <Map className={'esri_map'} onLoad={this.handleMapLoad} mapProperties={{ basemap: { portalItem: { id: "96cff8b8e48d45548833f19e29f09943" } } }} viewProperties={{ center: [this.state.lng, this.state.lat], zoom: 19 }} style={{ height: this.state.mode === 'static_image' ? 1 : 700 }}>
+                                    <GeeLayer detecting_tree_crowns={this.state.detecting_tree_crowns} blob_set={this.blob_set.bind(this)} visible={this.state.mode === 'gee_layer'} layers={'WWF/carbon-maps/raw-data/imagery'} bands={"b1,b2,b3"} copyright={this.state.gee_copyright} />
+                                    <WaybackLayer detecting_tree_crowns={this.state.detecting_tree_crowns} blob_set={this.blob_set.bind(this)} visible={this.state.mode === 'webtile_layer'} urlTemplate={this.state.wms_endpoint} copyright={this.state.wms_copyright} />
+                                    <GeojsonLayer feature_collection={this.state.feature_collection} visible={this.state.mode !== 'static_image' && this.state.show_crowns} show_boxes={this.state.show_boxes} show_masks={this.state.show_masks} show_scores={this.state.show_scores} show_areas={this.state.show_areas} area_range_value={this.state.area_range_value} score_range_value={this.state.score_range_value} />
+                                </Map>
                                 <div className={'imageBackground'}></div>
                                 {/* Detecting trees spinner */}
-                                <div className="status_box" style={{ display: this.state.detecting_tree_crowns ? "block": "none" }} >
-                                    <Sync sx={{ fontSize: 60 }} className={"spin"}/>
+                                <div className="status_box" style={{ display: this.state.detecting_tree_crowns ? "block" : "none" }} >
+                                    <Sync sx={{ fontSize: 60 }} className={"spin"} />
                                     <div className='status_text'>Detecting trees..</div>
-                                    <div className='geeLoading' style={{ 'display': (this.state.mode !== 'static_image' && this.state.getting_dynamic_image) ? 'block' : 'none'}}>{this.state.status_text}</div>
+                                    <div className='geeLoading' style={{ 'display': (this.state.mode !== 'static_image' && this.state.getting_dynamic_image) ? 'block' : 'none' }}>{this.state.status_text}</div>
                                 </div>
                                 {/* Loading imagery spinner */}
-                                <div className="status_box" style={{ display: (this.state.mode !== 'static_image' && this.state.getting_dynamic_image) ? "block": "none" }} >
-                                    <Sync sx={{ fontSize: 60 }} className={"spin"} style={{color: 'gray'}}/>
+                                <div className="status_box" style={{ display: (this.state.mode !== 'static_image' && this.state.getting_dynamic_image) ? "block" : "none" }} >
+                                    <Sync sx={{ fontSize: 60 }} className={"spin"} style={{ color: 'gray' }} />
                                     <div className='geeLoading'>{this.state.status_text}</div>
                                 </div>
                             </td>
                             <td className={'buttonCell'}>
                                 <div>
-                    	            <Button variant="contained" type="file" onClick={this.openFilePicker.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Upload drone image for tree detection'>Drone</Button>
-                    	            <span className='span'></span>
-                    	            <Button variant="contained" onClick={this.changeToGeeImage.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Use aerial imagery from Google Earth Engine for tree detection'>Aerial</Button>
-                    	            <span className='span'></span>
-                    	            <Button variant="contained" onClick={this.changeToMaxarImage.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Use Maxar WorldView2 imagery for tree detection'>Satelllite</Button>
-                    	            <span className='span'></span>
-                    	            <Button variant="contained" onClick={this.changeToWaybackImage.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Use ESRI Wayback for tree detection'>Wayback</Button>
-                    	            <span className='span'></span>
-                    	            <Button variant="contained" onClick={this.processDynamicImage.bind(this)} disabled={this.state.mode==='static_image' || this.state.getting_dynamic_image || this.state.detecting_tree_crowns} color='error' title='Start tree detection on the imagery'>Go</Button>
-                    	            <span className='span'></span>
+                                    <Button variant="contained" type="file" onClick={this.openFilePicker.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Upload drone image for tree detection'>Drone</Button>
+                                    <span className='span'></span>
+                                    <Button variant="contained" onClick={this.changeToGeeImage.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Use aerial imagery from Google Earth Engine for tree detection'>Aerial</Button>
+                                    <span className='span'></span>
+                                    <Button variant="contained" onClick={this.changeToMaxarImage.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Use Maxar WorldView2 imagery for tree detection'>Satelllite</Button>
+                                    <span className='span'></span>
+                                    <Button variant="contained" onClick={this.changeToWaybackImage.bind(this)} disabled={this.state.getting_dynamic_image || this.state.detecting_tree_crowns} title='Use ESRI Wayback for tree detection'>Wayback</Button>
+                                    <span className='span'></span>
+                                    <Button variant="contained" onClick={this.processDynamicImage.bind(this)} disabled={this.state.mode === 'static_image' || this.state.getting_dynamic_image || this.state.detecting_tree_crowns} color='error' title='Start tree detection on the imagery'>Go</Button>
+                                    <span className='span'></span>
                                     <IconButton aria-label="delete" color="primary" onClick={this.downloadInstances.bind(this)} disabled={!this.state.feature_collection} title='Download the detected trees as Geojson'>
                                         <DownloadIcon />
                                     </IconButton>
-                                    <TreeCrownMetrics mode={this.state.mode} feature_collection={this.state.feature_collection} changeCrowns={this.changeCrowns.bind(this)} changeBoxes={this.changeBoxes.bind(this)} changeMasks={this.changeMasks.bind(this)} changeScores={this.changeScores.bind(this)} changeAreas={this.changeAreas.bind(this)} show_crowns={this.state.show_crowns} show_boxes={this.state.show_boxes} show_masks={this.state.show_masks} show_scores={this.state.show_scores} show_areas={this.state.show_areas} change_area_range={this.change_area_range.bind(this)} area_range_value={this.state.area_range_value} score_range_value={this.state.score_range_value} change_score_range={this.change_score_range.bind(this)}/>
+                                    <TreeCrownMetrics mode={this.state.mode} feature_collection={this.state.feature_collection} changeCrowns={this.changeCrowns.bind(this)} changeBoxes={this.changeBoxes.bind(this)} changeMasks={this.changeMasks.bind(this)} changeScores={this.changeScores.bind(this)} changeAreas={this.changeAreas.bind(this)} show_crowns={this.state.show_crowns} show_boxes={this.state.show_boxes} show_masks={this.state.show_masks} show_scores={this.state.show_scores} show_areas={this.state.show_areas} change_area_range={this.change_area_range.bind(this)} area_range_value={this.state.area_range_value} score_range_value={this.state.score_range_value} change_score_range={this.change_score_range.bind(this)} />
                                 </div>
-                	        </td>
-                	   </tr>
-                       <tr><div className={'citation'}>{this.state.model_copyright}</div></tr>
-            	   </tbody>
-            	</table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className='model'>
+                    <span className={'modelname'}>
+                        <label htmlFor="model">Tree detection model: &nbsp;</label>
+                        <select name="model" id="model" onChange={e => this.changeModel(e.target.value)} value={this.state.model}>
+                            <option value="cambridge">Cambridge</option>
+                            <option value="restor">Restor</option>
+                        </select>
+                    </span>
+                    <span className={'citation'}>{this.state.model_copyright}</span>
+                </div>
             </div>
         );
     }
