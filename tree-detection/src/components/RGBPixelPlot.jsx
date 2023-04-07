cb553b3d4d6e8fe4470ad96e1001d71a00c6606d
Andrew Cottam <a.cottam@gmail.com>
2023-04-07 19:51:03 +0200

    pixel plot uses jsx syntax

--- tree-detection/src/components/RGBPixelPlot.jsx
+++ tree-detection/src/components/RGBPixelPlot.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// deck.gl imports
 import DeckGL from '@deck.gl/react';
 import { OrbitView } from '@deck.gl/core';
 import { PointCloudLayer } from '@deck.gl/layers';
@@ -7,36 +8,24 @@ import { COORDINATE_SYSTEM } from '@deck.gl/core';
 export default function RGBPixelPlot(props) {
 
     //local state
-    const [layer, setLayer] = useState(null);
-    const [view, setView] = useState(null);
+    const [data, setData] = useState(null);
     const [viewState, setViewState] = useState({});
 
-    if (props.canvas && !layer) {
+    if (props.canvas && !data) {
         const context = props.canvas.getContext("2d");
         // get the image pixel data as a 1 dimensional array (Uint8ClampedArray)
         const d = context.getImageData(0, 0, props.canvas.width, props.canvas.height).data;
-        let data = []
+        let _data = []
+        // create the point cloud data
         for (var i = 0; i < d.length; i += 4) {
-            data.push({ position: d.slice(i, i + 2), normal: [-1, 0, 0], color: [255, 255, 0] });
+            _data.push({ position: d.slice(i, i + 2), normal: [-1, 0, 0], color: [0, 255, 0] });
         }
-        // const data = [
-        //     { position: [-122, 37.7, 12], normal: [1, 0, 0], color: [255, 255, 0] },
-        //     { position: [122, 37, 14], normal: [1, 0, 0], color: [255, 0, 0] }
-        // ]
-        const lyr = new PointCloudLayer({
-            id: 'point-cloud-layer',
-            data,
-            coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-            pointSize: 2,
-            getPosition: d => d.position,
-            getNormal: d => d.normal,
-            getColor: d => d.color
-        });
-        const view = new OrbitView();
-        setLayer(lyr);
-        setView(view);
         setViewState({});
+        setData(_data);
     }
 
-    return <DeckGL layers={[layer]} view={view} width={600} height={600} viewState={viewState} />;
+    return <DeckGL width={600} height={600} initialViewState={viewState}>
+        <PointCloudLayer id='point-cloud-layer' data={data} coordinateSystem={COORDINATE_SYSTEM.CARTESIAN} pointSize={2}/>
+        <OrbitView />
+    </DeckGL>
 }
