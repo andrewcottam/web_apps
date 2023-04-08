bf607bba5a55c7e3f757620d718967db823c63d2
Andrew Cottam <a.cottam@gmail.com>
2023-04-08 13:34:00 +0200

    RGBPixelPlot working interactively

--- tree-detection/src/components/RGBPixelPlot.jsx
+++ tree-detection/src/components/RGBPixelPlot.jsx
@@ -1,31 +1,21 @@
-import React, { useState } from "react";
-// deck.gl imports
 import DeckGL from '@deck.gl/react';
-import { OrbitView } from '@deck.gl/core';
 import { PointCloudLayer } from '@deck.gl/layers';
-import { COORDINATE_SYSTEM } from '@deck.gl/core';
+import { OrbitView, COORDINATE_SYSTEM } from '@deck.gl/core';
 
-export default function RGBPixelPlot(props) {
+export default function App({ data, viewState }) {
+  
+  const INITIAL_VIEW_STATE = {
+    target: [0, 0, 0],
+    zoom: 4,
+    rotationOrbit: 0,
+    rotationX: 0,
+    minRotationX: -90,
+    maxRotationX: 90,
+    minZoom: -10,
+    maxZoom: 10
+  }
 
-    //local state
-    const [data, setData] = useState(null);
-    const [viewState, setViewState] = useState({});
-
-    if (props.canvas && !data) {
-        const context = props.canvas.getContext("2d");
-        // get the image pixel data as a 1 dimensional array (Uint8ClampedArray)
-        const d = context.getImageData(0, 0, props.canvas.width, props.canvas.height).data;
-        let _data = []
-        // create the point cloud data
-        for (var i = 0; i < d.length; i += 4) {
-            _data.push({ position: d.slice(i, i + 2), normal: [-1, 0, 0], color: [0, 255, 0] });
-        }
-        setViewState({});
-        setData(_data);
-    }
-
-    return <DeckGL width={600} height={600} initialViewState={viewState}>
-        <PointCloudLayer id='point-cloud-layer' data={data} coordinateSystem={COORDINATE_SYSTEM.CARTESIAN} pointSize={2}/>
-        <OrbitView />
-    </DeckGL>
+  return <DeckGL initialViewState={INITIAL_VIEW_STATE} views={[new OrbitView()]} controller={true} width={600}>
+    <PointCloudLayer data={data} coordinateSystem={COORDINATE_SYSTEM.CARTESIAN} pointSize={4} getPosition={d => d.position} getNormal={d => d.normal} getColor={d => d.color} />
+  </DeckGL>;
 }
--- vite-scratch/src/App.jsx
+++ vite-scratch/src/App.jsx
@@ -1,36 +1,21 @@
 import DeckGL from '@deck.gl/react';
-import {PointCloudLayer} from '@deck.gl/layers';
-import { COORDINATE_SYSTEM } from '@deck.gl/core';
+import { PointCloudLayer } from '@deck.gl/layers';
+import { OrbitView, COORDINATE_SYSTEM } from '@deck.gl/core';
 
-export default function App({data, viewState}) {
-  /**
-   * Data format:
-   * [
-   *   {position: [-122.4, 37.7, 12], normal: [-1, 0, 0], color: [255, 255, 0]},
-   *   ...
-   * ]
-   */
-  const INITIAL_VIEW_STATE = {
-    longitude: -122.41669,
-    latitude: 37.7853,
-    zoom: 1,
-    pitch: 0,
-    bearing: 0
-  };
+export default function App({ data, viewState }) {
   
-  const layer = new PointCloudLayer({
-    id: 'point-cloud-layer',
-    data: [{position: [-122.4, 37.7, 12], normal: [-1, 0, 0], color: [255, 255, 0]}],
-    pickable: false,
-    coordinateSystem: COORDINATE_SYSTEM.METER_OFFSETS,
-    coordinateOrigin: [-122.4, 37.74],
-    radiusPixels: 4,
-    getPosition: d => d.position,
-    getNormal: d => d.normal,
-    getColor: d => d.color
-  });
+  const INITIAL_VIEW_STATE = {
+    target: [0, 0, 0],
+    zoom: 4,
+    rotationOrbit: 0,
+    rotationX: 0,
+    minRotationX: -90,
+    maxRotationX: 90,
+    minZoom: -10,
+    maxZoom: 10
+  }
 
-  return <DeckGL initialViewState={INITIAL_VIEW_STATE}
-    layers={[layer]}
-    getTooltip={({object}) => object && object.position.join(', ')} controller={true}/>;
+  return <DeckGL initialViewState={INITIAL_VIEW_STATE} views={[new OrbitView()]} controller={true}>
+    <PointCloudLayer data={data} coordinateSystem={COORDINATE_SYSTEM.CARTESIAN} pointSize={4} getPosition={d => d.position} getNormal={d => d.normal} getColor={d => d.color} />
+  </DeckGL>;
 }
