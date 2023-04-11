839fee43c11c2698ffc9308f4d103a5207a4a097
Andrew Cottam <a.cottam@gmail.com>
2023-04-11 18:01:37 +0200

    first v of electricity grid working

--- vite-scratch/src/main.jsx
+++ vite-scratch/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App data={[{position: [0, 0, 12], normal: [0, 0, 0], color: [0, 255, 0]},{position: [1, 1, 12], normal: [0, 0, 0], color: [255, 0, 0]}]}/>
+    <App />
     {/* <App /> */}
   </React.StrictMode>,
 )
