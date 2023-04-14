f717e5fed4343dbe0563f34545c205b2f4350772
Andrew Cottam <a.cottam@gmail.com>
2023-04-14 12:14:03 +0200

    shaders sort of working with LineLayer

--- vite-scratch/src/App.jsx
+++ vite-scratch/src/App.jsx
@@ -1,21 +1,55 @@
 import DeckGL from '@deck.gl/react';
-import {GeoJsonLayer} from '@deck.gl/layers';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import PowerLineExtension from './components/PowerLineExtension';
+import PowerLineLayer from './components/PowerLineLayer';
 
 const DATA_URL = {
-  POWER_LINES: "https://raw.githubusercontent.com/andrewcottam/general/master/open%20street%20map%20power%20lines%20in%20england.geojson"
+  POWER_LINES: "https://raw.githubusercontent.com/andrewcottam/general/master/pwrlinesll2.geojson"
 };
 
 const INITIAL_VIEW_STATE = {
-  latitude: 54,
-  longitude: 0,
-  zoom: 7,
+  latitude: 52.207,
+  longitude: 0.315,
+  zoom: 13,
   minZoom: 2,
-  maxZoom: 18
+  maxZoom: 25
 };
 
-export default function App({data=DATA_URL.POWER_LINES }) {
+export default function App() {
 
+  function getData(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          const coords = json.features[0].geometry.coordinates;
+          const out = coords.map((item, i, arr) => {
+            return { from: arr[i], to: arr[i + 1] }
+          })
+          //remove the last item as it will have an end point of undefined
+          resolve(out.slice(0,out.length-1))
+        });
+    })
+  }
+  
   return <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
-    <GeoJsonLayer data={data} lineWidthMinPixels={0.5}/>
+    {/* <GeoJsonLayer
+      data={data}
+      extensions={[new PowerLineExtension()]}
+      lineWidthMinPixels={10}
+      getLineColor={[0, 200, 255]}
+      getLineLength={d => (d.properties.length)}
+      animationSpeed={2} tailLength={3}
+    /> */}
+    <PowerLineLayer
+      data={getData(DATA_URL.POWER_LINES)}
+      widthMinPixels={5}
+      getSourcePosition={d => d.from}
+      getTargetPosition={d => d.to}
+      getColor={[0, 200, 255]}
+      getFrequency={0.1}
+      animationSpeed={1}
+      tailLength={3}
+    />
   </DeckGL>;
 }
---
+++ vite-scratch/src/components/PowerLineLayer.jsx
@@ -0,0 +1,90 @@
+import { LineLayer } from '@deck.gl/layers';
+
+const defaultProps = {
+    // Length of power line
+    getLineLength: { type: 'accessor', value: 0 },
+    // Frequency of the running light
+    getFrequency: { type: 'accessor', value: 1 },
+    // Speed of the running light
+    animationSpeed: { type: 'number', min: 0, value: 1 },
+    // Size of the blob
+    tailLength: { type: 'number', min: 0, value: 1 }
+}
+
+// Shader definitions
+const vsDeclaration = `
+attribute float instanceFrequency;
+varying float vArcLength;
+varying float vFrequency;
+`
+
+const vsMain = `
+//this length depends on the zoom level and can be greater than 1
+vArcLength = distance(source, target);
+vFrequency = instanceFrequency;
+`
+
+const fsDeclaration = `
+uniform float tailLength;
+uniform float timestamp;
+uniform float animationSpeed;
+varying float vArcLength;
+varying float vFrequency;
+`
+
+const fsColorFilter = `
+//if vArcLength < 1 then tripDuration will be < 0.5 
+float tripDuration = vArcLength / animationSpeed;
+float flightInterval = 1.0 / vFrequency;
+float r = mod(geometry.uv.x, flightInterval);
+float rMax = mod(fract(timestamp / tripDuration), flightInterval);
+float rMin = rMax - tailLength / vArcLength;
+float alpha = (r > rMax ? 0.0 : smoothstep(rMin, rMax, r)) + smoothstep(rMin + flightInterval, rMax + flightInterval, r);
+if (alpha == 0.0) {
+  discard;
+}
+color.a *= alpha;
+`
+
+export default class PowerLineLayer extends LineLayer {
+
+    initializeState(params) {
+        super.initializeState(params);
+        this.getAttributeManager().addInstanced({
+            instanceFrequency: {
+                size: 1,
+                accessor: 'getFrequency',
+                defaultValue: 1
+            },
+        });
+    }
+
+    getShaders() {
+        const shaders = super.getShaders();
+        return {
+            ...super.getShaders(),
+            inject: {
+                'vs:#decl': vsDeclaration,
+                'vs:#main-end': vsMain,
+                'fs:#decl': fsDeclaration,
+                'fs:DECKGL_FILTER_COLOR': fsColorFilter
+            }
+        }
+    }
+
+    draw(opts) {
+        this.state.model.setUniforms({
+            tailLength: this.props.tailLength,
+            animationSpeed: this.props.animationSpeed,
+            //number of seconds since midnight
+            timestamp: (Date.now() / 1000) % 86400
+        });
+        super.draw(opts);
+        // By default, the needsRedraw flag is cleared at each render. We want the layer to continue
+        // refreshing.
+        this.setNeedsRedraw();
+    }
+}
+
+PowerLineLayer.layerName = 'PowerLineLayer';
+PowerLineLayer.defaultProps = defaultProps;
