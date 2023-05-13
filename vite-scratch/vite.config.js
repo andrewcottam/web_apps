2240253403cf8ea9d13546b80d8dc5f87f84e76c
Andrew Cottam <a.cottam@gmail.com>
2023-05-13 18:23:52 +0200

    added base to vite.config in vite-scratch

--- vite-scratch/vite.config.js
+++ vite-scratch/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/web_apps/vite-scratch/dist",
   plugins: [react()],
   server: {
     open: '/web_apps/vite-scratch/dist',
