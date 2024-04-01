d1ec6e26057b2f19f1551742d5977ff75c896508
andrewcottam <a.cottam@gmail.com>
2024-04-01 10:44:57 +0200

    added Gainforest cog

--- cog-clients/main.js
+++ cog-clients/main.js
@@ -5,7 +5,8 @@ import TileLayer from 'ol/layer/WebGLTile.js';
 const source = new GeoTIFF({
   sources: [
     {
-      url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
+      // url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
+      url: 'https://storage.googleapis.com/gainforest-public/odm_orthophoto2.tif',
     },
   ],
 });
