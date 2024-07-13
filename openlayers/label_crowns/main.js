628306c2aff773a5efc7045e42892312fe9c5dd8
andrewcottam <a.cottam@gmail.com>
2024-07-13 11:51:59 -0400

    backspace to download

--- openlayers/label_crowns/main.js
+++ openlayers/label_crowns/main.js
@@ -138,7 +138,7 @@ function downloadGeoJSON() {
 document.addEventListener('keydown', function (event) {
     // Your code here
     // console.log(`Key pressed: ${event.key}`);
-    if (event.key === 'd') {
+    if (event.key === 'Backspace') {
         downloadGeoJSON();
     }
     if (event.key === 'Enter') {
