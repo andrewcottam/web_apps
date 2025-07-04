7414d58f07e86cfbc34693ba5cfac5003acc5cc8
Andrew Cottam <a.cottam@gmail.com>
2025-07-04 09:58:00 +0100

    base update

--- openlayers/verify/vite.config.ts
+++ openlayers/verify/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/web_apps/openlayers/verify/',
+  base: '/web_apps/openlayers/verify/dist/',
   plugins: [react()],
 })
