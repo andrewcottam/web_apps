1d82a15d834832a1456bef6b07018b2c7cedd528
Andrew Cottam <a.cottam@gmail.com>
2025-07-28 11:39:48 +0200

    removed centroid from ui

--- openlayers/verify/src/App.tsx
+++ openlayers/verify/src/App.tsx
@@ -445,7 +445,7 @@ const App: React.FC = () => {
                   className="content_box"
                 >
                   {selectedTab === "Geometric" && data.geometric && (
-                    <JsonViewer data={{Area: data.geometric.area_str, 'Average Segment Length': data.geometric.average_segment_length_str, Centroid: data.geometric.centroid, 'Is valid': data.geometric.first_polygon_is_valid, Perimeter: data.geometric.perimeter_str, Vertices: data.geometric.points}} />
+                    <JsonViewer data={{Area: data.geometric.area_str, 'Average Segment Length': data.geometric.average_segment_length_str, 'Is valid': data.geometric.first_polygon_is_valid, Perimeter: data.geometric.perimeter_str, Vertices: data.geometric.points}} />
                   )}
 
                   {selectedTab === "Land Cover" && data.landcover && (
