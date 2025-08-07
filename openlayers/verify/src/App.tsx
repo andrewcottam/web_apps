8e48069839e83e4fe2f7b1845d9b91dcf0a6ea8c
Andrew Cottam <a.cottam@gmail.com>
2025-08-07 21:09:22 +0200

    using secure sites bucket fpr mvt

--- openlayers/verify/src/App.tsx
+++ openlayers/verify/src/App.tsx
@@ -272,7 +272,6 @@ const App: React.FC = () => {
       const isLocalhost = false;
       const wdpa_endpoint = isLocalhost
         ? "http://127.0.0.1:5000/tiles/{z}/{x}/{y}.pbf"
-        // : "https://mvt-server-468041596913.europe-west6.run.app/tiles/{z}/{x}/{y}.pbf";
         : "https://europe-west6-restor-gis.cloudfunctions.net/mvt_tile_server/tiles/{z}/{x}/{y}.pbf";
       const wdpa_source = new VectorTileSource({ format: new MVT(), url: wdpa_endpoint });
       const wdpa_layer = new VectorTileLayer({ source: wdpa_source, style: wdpa_style, minZoom: 10 });
@@ -282,7 +281,7 @@ const App: React.FC = () => {
       const sites_style = new Style({ fill: new Fill({ color: 'rgba(99, 148, 69, 0.0)', }), stroke: new Stroke({ color: [255, 0, 0, 0.3], width: 2 }) });
       const sites_endpoint = isLocalhost
         ? "http://127.0.0.1:5000/tiles/{z}/{x}/{y}.pbf"
-        : "https://mvt-server-sites-468041596913.europe-west6.run.app/tiles/{z}/{x}/{y}.pbf";
+        : "https://europe-west6-restor-gis.cloudfunctions.net/mvt_tile_server_secure/tiles/{z}/{x}/{y}.pbf";
       const sites_source = new VectorTileSource({ format: new MVT(), url: sites_endpoint });
       const sites_layer = new VectorTileLayer({ source: sites_source, style: sites_style, minZoom: 10 });
       map.addLayer(sites_layer);
