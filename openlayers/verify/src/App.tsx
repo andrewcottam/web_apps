2ffde5f4644c09973880f8b88b219fdc31ccaa68
Andrew Cottam <a.cottam@gmail.com>
2025-08-07 09:04:09 +0200

    compactness added

--- openlayers/verify/src/App.tsx
+++ openlayers/verify/src/App.tsx
@@ -501,7 +501,7 @@ const App: React.FC = () => {
                   className="content_box"
                 >
                   {selectedTab === "Geometric" && data.geometric && (
-                    <JsonViewer data={{ Area: data.geometric.area_str, 'Average Segment Length': data.geometric.average_segment_length_str, 'Is valid': data.geometric.is_valid, Perimeter: data.geometric.perimeter_str, Vertices: data.geometric.points }} />
+                    <JsonViewer data={{ Area: data.geometric.area_str, 'Average Segment Length': data.geometric.average_segment_length_str, Compactness: data.geometric.compactness, 'Is valid': data.geometric.is_valid, Perimeter: data.geometric.perimeter_str, Vertices: data.geometric.points }} />
                   )}
 
                   {selectedTab === "Land Cover" && data.landcover && (
