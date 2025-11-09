56bfa0c4c25676c4e859adeca00851c22486dc8f
Andrew Cottam <a.cottam@gmail.com>
2025-11-09 10:24:48 +0000

    vite build

--- openlayers/dist-alert/vite.config.ts
+++ openlayers/dist-alert/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/web_apps/openlayers/verify/dist/',
+  base: '/web_apps/openlayers/dist-alert/dist/',
   plugins: [react()],
 })
