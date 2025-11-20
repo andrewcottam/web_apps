6326ef478780899fef1608f77619f44e3400c880
Andrew Cottam <a.cottam@gmail.com>
2025-11-20 15:01:15 +0000

    dist folder for tree detection app

---
+++ tree-detection/dist/assets/calcite-action-c6869345.js
@@ -0,0 +1,5 @@
+import{A as t,d as o}from"./action-864371c3.js";import"./index-c1b02c2b.js";import"https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";import"https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";import"https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";import"./guid-9426053f.js";import"./interactive-573cbe88.js";import"./loadable-25ad9e4e.js";import"./t9n-3aafd3a8.js";import"./observers-4f67f1fb.js";import"./icon-7492116a.js";import"./loader-48a016c9.js";/*!
+ * All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+ * See https://github.com/Esri/calcite-components/blob/master/LICENSE.md for details.
+ * v1.0.8-next.4
+ */const A=t,C=o;export{A as CalciteAction,C as defineCustomElement};
---
+++ tree-detection/dist/assets/requestImageUtils-a24df2fd.js
@@ -0,0 +1,1 @@
+import{U as n}from"./index-c1b02c2b.js";async function r(a,t){const{data:e}=await n(a,{responseType:"image",...t});return e}export{r as t};
